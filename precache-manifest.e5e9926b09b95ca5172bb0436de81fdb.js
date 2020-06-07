2495f643a00300d108ec7386dbe3324ea365395c
Mukund Kedia <mukundkedia7478@pop-os.localdomain>
2020-06-07 17:43:09 +0530

    Updates

---
+++ precache-manifest.e5e9926b09b95ca5172bb0436de81fdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6efcdead34c349e20b1681bb6ff95c",
+    "url": "/metronome/index.html"
+  },
+  {
+    "revision": "a1f1911b6b5d725a0cd6",
+    "url": "/metronome/static/css/main.76b22918.chunk.css"
+  },
+  {
+    "revision": "9992219f6d42a0e45378",
+    "url": "/metronome/static/js/2.5de71022.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/metronome/static/js/2.5de71022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f1911b6b5d725a0cd6",
+    "url": "/metronome/static/js/main.28df023b.chunk.js"
+  },
+  {
+    "revision": "077a6ff90df08c0071bf",
+    "url": "/metronome/static/js/runtime-main.bd87557f.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
